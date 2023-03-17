9dad1147812899e3174cded49bfa34d12dbafe23
manjuvincent <manjuvincent@innovationsit.com>
2023-03-17 15:30:48 +0530

    m

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar";
 import { Home } from './components/pages/Home.js';
 import { About } from './components/pages/About.js';
 // import {Blog} from "./components/pages/Blog.js";
-// import {Contact} from "./components/pages/Contact.js";
+import {Contact} from "./components/pages/Contact.js";
 import {Accurate} from './components/pages/Accurate.js';
 import {RentSoftware } from './components/pages/RentSoftware.js';
 import {Network} from './components/pages/Network.js';
@@ -18,8 +18,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/About" element={<About />}></Route>
-          {/* <Route path="/Blog" element={<Blog />}></Route>
-          <Route path="/Contact" element={<Contact />}></Route> */}
+          {/* <Route path="/Blog" element={<Blog />}></Route> */}
+          <Route path="/Contact" element={<Contact />}></Route>
           <Route path="/Accurate" element={<Accurate />}></Route>
           <Route path="/Software" element={<RentSoftware />}></Route>
           <Route path="/Network" element={<Network />}></Route>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import './NavBar.css';
-import { NavLink } from "react-router-dom";
+import { NavLink,NavBtn,NavBtnLink } from "react-router-dom";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react';
 import { GiHamburgerMenu } from "react-icons/gi";
